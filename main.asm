8dc0436a6740a9ea1efe249b3d320e890bd8c4c3
skripat4 <skripat4@gmail.com>
2022-07-06 17:30:54 +0300

    Crazy Something

--- main.asm
+++ main.asm
@@ -1,5 +1,8 @@
 org $6000
-ld de,16384
+ld d,5
+ld e,3
+call GIVE_ADR
+push hl
 ld hl,theUdg
 ld b,8
 loop:
@@ -9,7 +12,28 @@ inc hl
 inc d
 djnz loop
 ld a,(hl)
-ld (22528),a
+;ld (22528),a
+pop hl
+ld (hl),a
 ret
 theudg:
 defb 24,36,114,249,251,126,126,60,20
+
+GIVE_ADR:
+PUSH AF
+
+LD a,e
+and $18
+or $40
+LD h,a
+ld a,e
+and 7
+or a
+RRA
+RRA
+RRA
+RRA
+add a,d
+ld l,a
+pop AF
+ret
